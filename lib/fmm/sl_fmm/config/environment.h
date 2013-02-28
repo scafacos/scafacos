c3797853b75b05f90bd71273413989a62f9bf669
Michael Hofmann <0@nospam.com>
2013-02-28 11:39:49 +0000

    sl_fmm: fix fine-grained debug/info/timing output
    git-svn-id: https://svn.version.fz-juelich.de/scafacos/trunk@2605 5161e1c8-67bf-11de-9fd5-51895aff932f

--- lib/fmm/sl_fmm/config/environment.h
+++ lib/fmm/sl_fmm/config/environment.h
@@ -1,10 +1,10 @@
 
-#ifdef FCS_ENABLE_TIMING
+#ifdef FCS_ENABLE_TIMING_SL_FMM
 # define SL_TIMING
 # define SL_TIMING_PRINT_PREFIX  "SL_FMM_TIMING: "
 # define SL_TIMING_PRINT(_i_, _s_, _n_, _v_, _r_)  Z_MOP(if ((_r_) == 0) SL_TIMING_PRINT_DEFAULT(_i_, _s_, _n_, _v_, _r_);)
 #endif
 
-#ifdef FCS_ENABLE_DEBUG
+#ifdef FCS_ENABLE_DEBUG_SL_FMM
 # define SLDEBUG_OUTPUT  5
 #endif
