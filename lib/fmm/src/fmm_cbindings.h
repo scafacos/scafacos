c1e5e3bceef03af502ff2c4a86ebbd4f183be065
Michael Hofmann <0@nospam.com>
2013-03-01 08:39:27 +0000

    fmm: fix previous commit
    git-svn-id: https://svn.version.fz-juelich.de/scafacos/trunk@2611 5161e1c8-67bf-11de-9fd5-51895aff932f

--- lib/fmm/src/fmm_cbindings.h
+++ lib/fmm/src/fmm_cbindings.h
@@ -18,8 +18,5 @@ void fmm_cfinalize(void*,long long);
 void fmm_cinitload(void *, fcs_float *, fcs_int);
 void fmm_csetload(void *, fcs_float);
 
-void fmm_cinitresort(void *, fcs_fmm_resort_t);
-void fmm_csetresort(void *, long long);
-
 
 #endif /* __FMM_CBINDINGS_H__ */
