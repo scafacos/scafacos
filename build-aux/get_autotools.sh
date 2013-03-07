352d324ac53e288c2b4357ee2f07ba7090a1a368
Matthias Winkel <0@nospam.com>
2013-03-07 11:22:47 +0000

    refurbished get_autotools.sh and increased automake version as pepc might need it soon
    git-svn-id: https://svn.version.fz-juelich.de/scafacos/trunk@2629 5161e1c8-67bf-11de-9fd5-51895aff932f

--- build-aux/get_autotools.sh
+++ build-aux/get_autotools.sh
@@ -1,40 +1,46 @@
 # Assume we want to install them below $HOME/local.
-myprefix=$HOME/local
+myprefix=${HOME}/local
+tempdir="./autotools.tmp"
 
 # Show where the auto-tools will be installed to
 echo "auto-tools will be installed to: $myprefix"
 
 # Create a temporary directory to store the tar-balls
 echo "creating temporary directory"
-mkdir tmp
-cd tmp
+mkdir ${tempdir}
+cd ${tempdir}
+
+M4=m4-1.4.16
+AUTOCONF=autoconf-2.68
+AUTOMAKE=automake-1.12.3
+LIBTOOL=libtool-2.4.2
 
 # Do the following in a scratch directory.
 echo "downloading auto-tools"
-wget http://ftp.gnu.org/gnu/m4/m4-1.4.16.tar.gz
-wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.68.tar.gz
-wget http://ftp.gnu.org/gnu/automake/automake-1.11.3.tar.gz
-wget http://ftp.gnu.org/gnu/libtool/libtool-2.4.2.tar.gz
+wget http://ftp.gnu.org/gnu/m4/${M4}.tar.gz
+wget http://ftp.gnu.org/gnu/autoconf/${AUTOCONF}.tar.gz
+wget http://ftp.gnu.org/gnu/automake/${AUTOMAKE}.tar.gz
+wget http://ftp.gnu.org/gnu/libtool/${LIBTOOL}.tar.gz
 echo "extracting auto-tools"
-gzip -dc m4-1.4.16.tar.gz | tar xvf -
-gzip -dc autoconf-2.68.tar.gz | tar xvf -
-gzip -dc automake-1.11.3.tar.gz | tar xvf -
-gzip -dc libtool-2.4.2.tar.gz | tar xvf -
+gzip -dc ${M4}.tar.gz | tar xvf -
+gzip -dc ${AUTOCONF}.tar.gz | tar xvf -
+gzip -dc ${AUTOMAKE}.tar.gz | tar xvf -
+gzip -dc ${LIBTOOL}.tar.gz | tar xvf -
 echo "installing auto-tools to $myprefix"
-cd m4-1.4.16
+cd ${M4}
 ./configure -C --prefix=$myprefix && make && make install
 export PATH=${myprefix}:${PATH}
-cd ../autoconf-2.68
+cd ../${AUTOCONF}
 ./configure -C --prefix=$myprefix && make && make install
-cd ../automake-1.11.3
+cd ../${AUTOMAKE}
 ./configure -C --prefix=$myprefix && make && make install
-cd ../libtool-2.4.2
+cd ../${LIBTOOL}
 ./configure -C --prefix=$myprefix && make && make install
 
 # Clean up the temporary directory and downloaded tar-balls
 cd ../..
-echo "deleting temporary directory `pwd`/tmp"
-rm -rf tmp
+#echo "deleting temporary directory `pwd`/${tempdir}"
+#rm -rf ${tempdir}
 
 # Notification to update the PATH variable
 echo "$myprefix/bin was added to your PATH variable to use the newly installed auto-tools"
