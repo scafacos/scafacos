166ae09e8f801a785c2afdb838a87a560aeefc69
Michael <mhofma@googlemail.com>
2013-10-05 00:15:42 +0200

    fmm: switch to libtool

--- lib/fmm/sl_fmm/sl_fmm_config.sh
+++ lib/fmm/sl_fmm/sl_fmm_config.sh
@@ -3,4 +3,4 @@
 
 SL_CONFIG=../../common/sl/support/sl_config.sh
 
-${SL_CONFIG} --dst-sl=. --source-ref=../../common/sl --am-libname=libsl_fmm.a --no-am-libtool --extra-prefix=fcs_fmm_
+${SL_CONFIG} --dst-sl=. --source-ref=../../common/sl --am-libname=libsl_fmm.a --am-libprefix=fcs_fmm_ --extra-prefix=fcs_fmm_
