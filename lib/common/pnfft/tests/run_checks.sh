1177c50cbab02ee8a76e85fa15fc311ad6893c9f
Benedikt Morbach <benedikt.morbach@googlemail.com>
2013-10-20 16:01:46 +0200

    pnfft: allow tests to be run from any directory

--- lib/common/pnfft/tests/run_checks.sh
+++ lib/common/pnfft/tests/run_checks.sh
@@ -10,22 +10,22 @@ test_files_2="$test_files check_trafo_2d"
 test_files_8="$test_files check_trafo check_trafo_grad"
 test_files_8="$test_files pnfft_test pnfft_test_adv"
 
-
+testpath=`dirname ${0}`
  
 ## Run tests with 2 processes
 for name in $test_files_2; do
   echo "## mpirun -np 2 ${name}:"
-  mpirun -np 2 ./${name}
+  mpirun -np 2 ${testpath}/${name}
 done
 
 ## Run tests with 4 processes
 for name in $test_files_4; do
   echo "## mpirun -np 4 ${name}:"
-  mpirun -np 4 ./${name}
+  mpirun -np 4 ${testpath}/${name}
 done
 
 ## Run tests with 8 processes
 for name in $test_files_8; do
   echo "## mpirun -np 8 ${name}:"
-  mpirun -np 8 ./${name}
+  mpirun -np 8 ${testpath}/${name}
 done
