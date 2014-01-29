f102c990569bac7c4b38de9dcdaf0f71a7421657
Olaf Lenz <olenz@icp.uni-stuttgart.de>
2014-01-29 18:31:36 +0100

    Updated tests.

--- lib/p3m/src/tests/compute_error.cpp
+++ lib/p3m/src/tests/compute_error.cpp
@@ -9,7 +9,10 @@ using namespace ScaFaCoS::P3M;
 int main(int argc, char *argv[]) {
   MPI_Init(&argc, &argv);
 
-  if (argc <= 1) return 2;
+  if (argc <= 1) {
+    printf("Usage: compute_error <grid>\n");
+    return 2;
+  }
   fcs_int grid = atoi(argv[1]);
 
   data_struct d;
@@ -23,7 +26,7 @@ int main(int argc, char *argv[]) {
   d.sum_q2 = 300.0;
   d.square_sum_q = 0.0;
 
-  d.cao = 5;
+  d.cao = 4;
 
   d.grid[0] = grid;
   d.grid[1] = grid;
@@ -33,8 +36,8 @@ int main(int argc, char *argv[]) {
     k_space_error(&d);
     
     fcs_float alpha_L = d.alpha*d.box_l[0];
-    fcs_float Q = sqrt(d.ks_error)/(d.box_l[0]*d.box_l[0]);
-    printf("%lf %lf %lf %lf\n", d.alpha, d.ks_error, alpha_L, Q);
+    fcs_float Q = sqrt(d.ks_error/(d.box_l[0]*d.box_l[0]));
+    printf("%le %le %le %le\n", d.alpha, d.ks_error, alpha_L, Q);
   }
 
   MPI_Finalize();
