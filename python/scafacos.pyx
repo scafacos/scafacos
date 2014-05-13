64bca66266a3c07257ed34ea2f49da1e8a91ca2d
Olaf Lenz <olaf@lenz.name>
2014-05-13 10:57:18 +0200

    Implemented Python interface

--- python/scafacos.pyx
+++ python/scafacos.pyx
@@ -1,4 +1,6 @@
 cimport numpy as np
+import numpy as np
+from libc.string cimport memcpy
 
 # expose internal C-types to Cython
 # MPI stuff
@@ -33,26 +35,108 @@ cdef handleResult(FCSResult result):
 
 # FCS type
 cdef extern from "fcs.h":
-  cdef struct FCS_t:
-    pass
-  ctypedef FCS_t* FCS
-  FCSResult fcs_init(FCS* handle, const char* method, MPI_Comm communicator)
-  FCSResult fcs_destroy(FCS handle)
-  FCSResult fcs_tune (FCS handle, fcs_int local_particles, fcs_int local_max_particles, fcs_float *positions,  fcs_float *charges)
+    cdef struct FCS_t:
+        pass
+    ctypedef FCS_t* FCS
 
+    # constructor and destructor
+    FCSResult fcs_init(FCS* handle, const char* method, MPI_Comm communicator)
+    FCSResult fcs_destroy(FCS handle)
+
+    # different properties
+    const char* fcs_get_method_name(FCS handle)
+    FCSResult fcs_set_near_field_flag(FCS handle, fcs_int near_field_flag)
+    fcs_int fcs_get_near_field_flag(FCS handle)
+
+    # box vectors
+    FCSResult fcs_set_box_a(FCS handle, const fcs_float *box_a)
+    const fcs_float* fcs_get_box_a(FCS handle)
+    FCSResult fcs_set_box_b(FCS handle, const fcs_float *box_b)
+    const fcs_float* fcs_get_box_b(FCS handle)
+    FCSResult fcs_set_box_c(FCS handle, const fcs_float *box_c)
+    const fcs_float* fcs_get_box_c(FCS handle)
+    FCSResult fcs_set_periodicity(FCS handle, const fcs_int *periodicity)
+    const fcs_int *fcs_get_periodicity(FCS handle)
+  
+    # tune
+    FCSResult fcs_tune (FCS handle, fcs_int local_particles,
+                        fcs_float *positions,  fcs_float *charges)
+
+    FCSResult fcs_run(FCS handle, fcs_int local_particles,
+                      fcs_float *positions, fcs_float *charges,
+                      fcs_float *fields, fcs_float *potentials)
+
+  
 cdef class scafacos:
     cdef FCS handle
 
     def __init__(self, method):
         handleResult(fcs_init(&self.handle, method, MPI_COMM_WORLD))
+        self.near_field_flag = False
 
+    property method:
+        def __get__(self):
+            return fcs_get_method_name(self.handle)
+
+    property box:
+        def __set__(self, np.ndarray[fcs_float, ndim=2, mode='c'] box):
+            handleResult(fcs_set_box_a(self.handle, &box[0,0]))
+            handleResult(fcs_set_box_b(self.handle, &box[1,0]))
+            handleResult(fcs_set_box_c(self.handle, &box[2,0]))
+
+        def __get__(self):
+            cdef np.ndarray[fcs_float, ndim=2, mode='c'] box = np.empty((3,3))
+            cdef const fcs_float* a = fcs_get_box_a(self.handle)
+            cdef const fcs_float* b = fcs_get_box_b(self.handle)
+            cdef const fcs_float* c = fcs_get_box_c(self.handle)
+            memcpy(&box[0,0], a, 3*sizeof(fcs_float))
+            memcpy(&box[1,0], b, 3*sizeof(fcs_float))
+            memcpy(&box[2,0], c, 3*sizeof(fcs_float))
+            return box
+
+    property periodicity:
+        def __set__(self, p):
+            cdef fcs_int cp[3]
+            if p[0]: cp[0] = 1
+            else: cp[0] = 0
+            if p[1]: cp[1] = 1
+            else: cp[1] = 0
+            if p[2]: cp[2] = 1
+            else: cp[2] = 0
+            handleResult(fcs_set_periodicity(self.handle, cp))
+
+        def __get__(self):
+            cdef const fcs_int* p = fcs_get_periodicity(self.handle)
+            return bool(p[0]), bool(p[1]), bool(p[2])
+
+    property near_field_flag:
+        def __set__(self, f):
+            cdef fcs_int cf = int(f)
+            fcs_set_near_field_flag(self.handle, cf)
+
+        def __get__(self):
+            return bool(fcs_get_near_field_flag(self.handle))
+        
     def tune(self,
              np.ndarray[fcs_float, ndim=2, mode='c'] positions not None,
              np.ndarray[fcs_float, ndim=1, mode='c'] charges not None):
         N = positions.shape[1]
         if N != charges.shape[0]:
             raise Exception("The number of charges and positions must be equal!")
-        handleResult(fcs_tune(self.handle, N, N, &positions[0,0], &charges[0]))
+        handleResult(fcs_tune(self.handle, N, &positions[0,0], &charges[0]))
+
+    def __call__(self,
+            np.ndarray[fcs_float, ndim=2, mode='c'] positions not None,
+            np.ndarray[fcs_float, ndim=1, mode='c'] charges not None):
+        N = positions.shape[1]
+        if N != charges.shape[0]:
+            raise Exception("The number of charges and positions must be equal!")
+
+        cdef np.ndarray[fcs_float, ndim=2, mode='c'] fields = np.empty_like(positions)
+        cdef np.ndarray[fcs_float, ndim=1, mode='c'] potentials = np.empty_like(charges)
+        
+        handleResult(fcs_run(self.handle, N, &positions[0,0], &charges[0],
+                             &fields[0,0], &potentials[0]))
         
     def __del__(self):
         handleResult(fcs_destroy(self.handle))
--- python/test.py
+++ python/test.py
@@ -2,8 +2,21 @@ import mpi4py.MPI
 import scafacos
 import numpy
 
-s = scafacos.scafacos("p3m")
+p3m = scafacos.scafacos("p3m")
+print p3m.method
 
-positions = numpy.array([[0.5, 0.5], [2.0, 2.0]], dtype='double')
+p3m.box = numpy.array([[2.0, 0.0, 0.0],
+                        [0.0, 2.0, 0.0],
+                        [0.0, 0.0, 2.0]])
+print p3m.box
+
+p3m.periodicity = (True, True, True)
+print p3m.periodicity
+
+p3m.near_field_flag = False
+
+positions = numpy.array([[1.0, 1.0], [0.5, 1.5], [1.0, 1.0]], dtype='double')
 charges = numpy.array([1.0, -1.0], dtype='double')
-s.tune(positions, charges)
+p3m.tune(positions, charges)
+
+fields, potentials = p3m(positions, charges)
