c9e2c8f34b4edaebd49da9d1a928baa7062598d0
Olaf Lenz <olaf@lenz.name>
2014-05-14 18:28:53 +0200

    Basic integration of python into the build system.

--- python/setup.py
+++ python/setup.py
@@ -1,24 +1,43 @@
 #!/usr/bin/env python
 
-# To build the scafacos python wrapper,
-# * build ScaFaCoS with the configure option "--enable-single-lib --with-pic"
-# * adapt the following paths (builddir might be the same as srcdir)
-srcdir = '/home/olenz/projects/scafacos/src'
-builddir = '/home/olenz/projects/scafacos/obj.lancre/icp'
+# To build the scafacos python interface
+# * build ScaFaCoS with the configure option "--with-pic"
+# * first *install* the ScaFaCoS library
+# * make sure pkg-config can find scafacos (execute "pkg-config --libs scafacos")
+# * if not, add /path/to/scafacos/lib/pkgconfig to the environment
+#   variable PKG_CONFIG_PATH
+# * run "python setup.py build_ext -fi" to build the python module
 
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
 import os
+import commands
 
-ext_params = {}
-ext_params['include_dirs'] = ['{}/src'.format(srcdir), builddir, numpy.get_include()] 
-ext_params['library_dirs'] = ['{}/package/.libs'.format(builddir), '/usr/lib64']
-ext_params['libraries'] = ['fcs', 'mpi', 'mpi_cxx', 'fftw3', 'gsl', 'gslcblas']
+def pkgconfig(package, **kw):
+    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+    cmd = "pkg-config --libs --cflags {}".format(package)
+    status, output = commands.getstatusoutput(cmd)
+    if status != 0:
+        raise Exception(output)
+    tokens = output.split()
+    for token in tokens:
+        key = token[:2]
+        value = token[2:]
+        if flag_map.has_key(key):
+            kw.setdefault(flag_map[key], []).append(value)
+        else: # throw others to extra_link_args
+            kw.setdefault('extra_link_args', []).append(token)
+    return kw
+
+scafacos_params = pkgconfig("scafacos")
+scafacos_params.setdefault('include_dirs', []).append(numpy.get_include())
+
+print(scafacos_params.setdefault('libraries', []))
 
 ext_modules=[
-    Extension("scafacos", ["scafacos.pyx"], **ext_params),
+    Extension("scafacos", ["scafacos.pyx"], **scafacos_params),
 ]
 
 setup(
--- python/test.py
+++ python/test.py
@@ -1,22 +1,24 @@
+from __future__ import print_function
 import mpi4py.MPI
 import scafacos
 import numpy
 
 p3m = scafacos.scafacos("p3m")
-print p3m.method
-
 p3m.box = numpy.array([[2.0, 0.0, 0.0],
                         [0.0, 2.0, 0.0],
                         [0.0, 0.0, 2.0]])
-print p3m.box
-
 p3m.periodicity = (True, True, True)
-print p3m.periodicity
+p3m.near_field_flag = True
+p3m.total_particles = 2
 
-p3m.near_field_flag = False
-
-positions = numpy.array([[1.0, 1.0], [0.5, 1.5], [1.0, 1.0]], dtype='double')
+positions = numpy.array([[0.5, 1.0, 1.0], [1.5, 1.0, 1.0]], dtype='double')
+print(positions)
 charges = numpy.array([1.0, -1.0], dtype='double')
-p3m.tune(positions, charges)
 
+print(positions.reshape(6))
+
+p3m.tune(positions, charges)
 fields, potentials = p3m(positions, charges)
+
+print("fields={}".format(fields))
+print("potentials={}".format(potentials))
