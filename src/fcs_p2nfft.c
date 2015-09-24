55eeb319cf250401025d5553c99fe99ed3a6c52d
Michael Pippig <michael.pippig@mathematik.tu-chemnitz.de>
2015-09-24 10:41:12 +0200

    turn on P2NFFT dipole support

--- src/fcs_p2nfft.c
+++ src/fcs_p2nfft.c
@@ -93,6 +93,7 @@ FCSResult fcs_p2nfft_init(
   if (result != NULL)
     return result;
 
+  handle->dipole_support = FCS_TRUE;
   handle->destroy = fcs_p2nfft_destroy;
   handle->set_r_cut = fcs_p2nfft_set_r_cut;
   handle->unset_r_cut = fcs_p2nfft_set_r_cut_tune;
